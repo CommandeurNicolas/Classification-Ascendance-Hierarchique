9166382e0fa4fb95c8d67f5ecc57b8832d260900
Visar <Visar@DESKTOP-8A09G93>
2022-01-10 14:00:31 +0100

    data.txt

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+# argecho.py
+import sys
+
 if __name__ == "__main__":
-    print("coucou")
+    if(len(sys.argv) < 2):
+        print('usage: python main.py [file]')
+        exit(0)
+    file = open(sys.argv[1])
+    for line in file:
+        print(line,end='')
